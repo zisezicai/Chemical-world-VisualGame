833184106e0392dd34066e40f7d976d9b6767ab7
zisezicai <liuzezi@qq.com>
2026-06-20 20:26:18 +0800

    添加美术素材
    画了些美术素材，包括：
    游戏的草地
    玩家动画
    同时修复了玩家动画播放错误的问题

--- Scripts/Game/Player/Player.cs
+++ Scripts/Game/Player/Player.cs
@@ -1,9 +1,9 @@
 using Godot;
 using System;
 
-public partial class Player : Sprite2D
+public partial class Player : AnimatedSprite2D
 {
-	// Called when the node enters the scene tree for the first time.
+	public int speed=>10;
 	public override void _Ready()
 	{
 		this.GetNode<Label>("mainCamera/nameLabel").Text=SaveManager.Instance.playerName;
@@ -12,5 +12,42 @@ public partial class Player : Sprite2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		move();
+	}
+
+	public void move()//移动的逻辑
+	{
+		if (Input.IsActionPressed("up"))
+		{
+			// if(Input.IsActionJustPressed("up")){//只在第一帧开始播
+				this.Play("up");
+			// }
+			this.Position += new Vector2(0, -speed);
+		}
+		else if (Input.IsActionPressed("down"))
+		{
+			// if(Input.IsActionJustPressed("down")){
+				this.Play("down");
+			// }
+			this.Position += new Vector2(0, speed);
+		}
+		else if (Input.IsActionPressed("left"))
+		{
+			// if(Input.IsActionJustPressed("left")){
+				this.Play("left");
+			// }
+			this.Position += new Vector2(-speed, 0);
+		}
+		else if (Input.IsActionPressed("right"))
+		{
+			// if(Input.IsActionJustPressed("right")){
+				this.Play("right");
+			// }
+			this.Position += new Vector2(speed, 0);
+		}
+		else
+		{
+			this.Pause();
+		}
 	}
 }
