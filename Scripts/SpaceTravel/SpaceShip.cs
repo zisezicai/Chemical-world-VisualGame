a44a256feaa42f2d25036678956cff294fec9ff6
zisezicai <liuzezi@qq.com>
2026-07-19 13:38:08 +0800

    添加星球:地球
    添加地球图片，同时，玩家还会有离开地球的动画

--- Scripts/SpaceTravel/SpaceShip.cs
+++ Scripts/SpaceTravel/SpaceShip.cs
@@ -8,8 +8,24 @@ public partial class SpaceShip : CharacterBody2D
 	[Export]
 	public float rotationSpeed = 3.0f;
 
+
+    public override void _Ready()
+    {
+        this.Scale=new Vector2(0.05f,0.05f);
+    }
+
+    public override void _Process(double delta)
+    {
+        if (this.Scale.X < 1)
+        {
+            if(this.Scale.X>=0.95f) this.Scale=new Vector2(1,1);
+            else this.Scale=new Vector2(this.Scale.X+(float)(0.3f*delta),this.Scale.Y+(float)(0.3f*delta));
+        }
+    }
+
     public override void _PhysicsProcess(double delta)
     {
+        if(this.Scale.X<1) return;
         float moveVertical = Input.GetAxis("up", "down");
         float rotateHorizontal = Input.GetAxis("left", "right");
         if (rotateHorizontal != 0)
